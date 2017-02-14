f65ac1db7ac77bbe658c839115f6295d73ac78da
RenukaJ <RenukaJ>
2017-02-14 12:10:05 -0800

    Given an integer, write a function to determine if it is a power of three

---
+++ PowerofThree.java
@@ -0,0 +1,25 @@
+/*
+ * 326. Power of Three
+ * Given an integer, write a function to determine if it is a power of three.
+ */
+
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==1) return true;
+ 
+    boolean result = false;
+ 
+    while(n>0){
+        int m = n%3;
+        if(m==0){
+            n=n/3;
+            if(n==1)
+                return true;
+        }else{
+            return false;
+        }
+    }
+ 
+    return result;
+    }
+}
