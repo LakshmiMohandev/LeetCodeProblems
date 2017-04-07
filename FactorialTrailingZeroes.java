0d1e5ca883dfff96b4aae3acff69e96428e4b113
RenukaJ <RenukaJ>
2017-04-06 20:37:25 -0700

    Given an integer n, return the number of trailing zeroes in n!.

---
+++ FactorialTrailingZeroes.java
@@ -0,0 +1,14 @@
+/*
+ * 172. Factorial Trailing Zeroes
+ * Given an integer n, return the number of trailing zeroes in n!.*/
+
+public class Solution {
+    public int trailingZeroes(int n) {
+        int cnt = 0;
+        while(n>0){
+            cnt += n/5;
+            n/=5;
+        }
+        return cnt;
+    }
+}
